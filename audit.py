c23c59c8cda9ad7814422a470ebf273d20cd998a
BohdanM19 <bogdanrichers@gmail.com>
2019-10-27 18:53:55 +0200

    complete!

--- audit.py
+++ audit.py
@@ -2,20 +2,20 @@ import requests
 import json
 
 
-def append_to_json(_dict, path):
-    with open(path, 'ab+') as f:
-        f.seek(0, 2)  # Go to the end of file
-        if f.tell() == 0:  # Check if file is empty
-            f.write(json.dumps([_dict], indent=4).encode())  # If empty, write an array
-        else:
-            f.seek(-1, 2)
-            f.truncate()  # Remove the last character, open the array
-            f.write(' , '.encode())  # Write the separator
-            f.write(json.dumps(_dict, indent=4).encode())  # Dump the dictionary
-            f.write(']'.encode())  # Close the array
-
-
 class Audit():
+    @staticmethod
+    def append_to_json(_dict, path):
+        with open(path, 'ab+') as f:
+            f.seek(0, 2)  # Go to the end of file
+            if f.tell() == 0:  # Check if file is empty
+                f.write(json.dumps([_dict], indent=4).encode())  # If empty, write an array
+            else:
+                f.seek(-1, 2)
+                f.truncate()  # Remove the last character, open the array
+                f.write(' , '.encode())  # Write the separator
+                f.write(json.dumps(_dict, indent=4).encode())  # Dump the dictionary
+                f.write(']'.encode())  # Close the array
+
     def __init__(self, url):
         self.base_url = url
         self.client = requests.Session()
@@ -32,9 +32,19 @@ class Audit():
         return info
 
 
+class AuditTask():
+    def __init__(self, base_url):
+        self.base_url = base_url
+        self.audit = Audit(url=self.base_url)
+
+    def search_url_in_cache(self, search_url):
+        response = self.audit.do_request(search_url)
+        data = self.audit.process_response(response, search_url)
+        self.audit.append_to_json(data, "info.json")
+
+
 url = "https://webcache.googleusercontent.com/search?q=cache:{}"
 search_url = "facebook.com"
-task = Audit(url=url)
-response = task.do_request(search_url)
-data = task.process_response(response, search_url)
-append_to_json(data, "info.json")
+
+task = AuditTask(base_url=url)
+task.search_url_in_cache(search_url=search_url)
