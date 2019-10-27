c4e31993aa504d2b81e8d8cb79b2d68000999f69
BohdanM19 <bogdanrichers@gmail.com>
2019-10-27 17:59:42 +0200

    Update

--- audit.py
+++ audit.py
@@ -9,9 +9,18 @@ class Audit():
 
     def do_request(self, url):
         req_url = self.base_url.format(url)
-        with self.client.get(url=req_url) as response:
-            text = response.text
+        with self.client.get(url=req_url) as resp:
+            return resp
+
+    @staticmethod
+    def process_response(response, search_url):
+        info = {"Search Url": search_url}
+        info['Exist'] = True if response.status_code == 200 else False
+        if response.code == 200:
+            pass
 
 
 a = Audit()
-a.do_request("http://twitdfgdfgch.tv")
+b = a.do_request("youtube.com")
+a.process_response(b, "youtube")
+
